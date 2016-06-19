96fb62786285946b503986cb9f0866b97d6f56a8
anthony-y <anthonyybayn@gmail.com>
2016-06-19 14:14:09 +0100

    Added config.bat so you can change the build config for both run.bat and compile.bat in one place.

---
+++ config.bat
@@ -0,0 +1,1 @@
+SET config=Release
--- run.bat
+++ run.bat
@@ -2,16 +2,19 @@
 
 cls
 
+call config.bat
+
 echo Executing!
 echo(
 echo ================================
 echo(
 
-cd Builds/SharpECS.Samples/Release
+cd Builds/SharpECS.Samples/%config%
 SharpECS.Samples.exe
 
 echo(
 echo ================================
 echo(
+cd ..
 echo Done!
 echo(
